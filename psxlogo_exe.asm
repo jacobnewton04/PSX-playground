9bff28e7a608d53bc21c1412856f3d128a9c17b3
Jacob <jacob.newton@me.com>
2025-11-01 17:28:02 -0500

    fixes

--- psxlogo_exe.asm
+++ psxlogo_exe.asm
@@ -1,4 +1,4 @@
-; psxlogo_exe.asm
+; psxlogo_exe.asm — your logo playground as a PS-X EXE
 ; assemble: armips psxlogo_exe.asm
 ; output:   psxlogo.exe
 ; load @ 0x80010000, entry @ 0x80010000
@@ -203,4 +203,60 @@ DrawFrame:
     ; ------------- rect 1 -------------
     li      $t4, 0x60000000
     or      $t4, $t4, $t9
+    sw      $t4, 0($t3)             ; cmd+color
+
+    sll     $t1, $t6, 16            ; y << 16
+    or      $t1, $t1, $t5           ; x | (y<<16)
+    sw      $t1, 0($t3)             ; xy
+
+    li      $t1, 28 | (80 << 16)    ; w=28, h=80
+    sw      $t1, 0($t3)             ; wh
+
+    ; ------------- rect 2 -------------
+    li      $t4, 0x60000000
+    or      $t4, $t4, $t9
     sw      $t4, 0($t3)
+
+    addiu   $t0, $t5, 20            ; x2 = x + 20
+    addiu   $t1, $t6, 50            ; y2 = y + 50
+    sll     $t1, $t1, 16
+    or      $t1, $t1, $t0
+    sw      $t1, 0($t3)
+
+    li      $t1, 20 | (20 << 16)    ; w=20, h=20
+    sw      $t1, 0($t3)
+
+; --------------------------------------------
+; delay
+; --------------------------------------------
+DelayLoop:
+    li      $t0, 30000
+DL1:
+    addiu   $t0, $t0, -1
+    bgtz    $t0, DL1
+    nop
+
+    j       MainLoop
+    nop
+
+; --------------------------------------------
+; data section
+; --------------------------------------------
+.align 4
+PadBuf1:
+    .fill PAD_BUFSZ, 0
+PadBuf2:
+    .fill PAD_BUFSZ, 0
+
+LogoX:      .word 0
+LogoY:      .word 0
+ColorIndex: .word 0
+
+LogoColors:
+    .word 0x000000FF
+    .word 0x0000FF00
+    .word 0x00FF0000
+    .word 0x00FFFFFF
+
+CodeEnd:
+.close
